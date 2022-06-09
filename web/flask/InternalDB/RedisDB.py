a5e813d8db9dcc5aa91fd4dad3fd3262acbf8a75
CW-B-W <cw.brad.wang@gmail.com>
2022-06-09 11:43:06 +0000

    Separete RedisDB from datafabric.py

---
+++ web/flask/InternalDB/RedisDB.py
@@ -0,0 +1,36 @@
+import redis
+import json
+class RedisDB:
+    def __init__(self, db):
+        self.__redis_db = None
+        self.__host     = 'datafabric-redis'
+        self.__port     = 6379
+        self.__db       = db
+
+    def __connect_redis(self, retry=3) -> bool:
+        if self.__redis_db is not None:
+            return True
+        while retry >= 0:
+            try:
+                self.__redis_db = redis.Redis(host=self.__host, port=self.__port, db=self.__db)
+                return True
+            except Exception as ex:
+                if retry == 0:
+                    raise ex
+                retry -= 1
+        return False
+
+    def set_json(self, key: str, obj: dict, expire: int = None) -> bool:
+        if self.__connect_redis() == True:
+            self.__redis_db.set(key, json.dumps(obj))
+            if expire is not None:
+                self.__redis_db.expire(key, expire)
+            return True
+        else:
+            return False
+
+    def get_json(self, key: str) -> dict:
+        if self.__connect_redis() == True and self.__redis_db.exists(key):
+            return json.loads(self.__redis_db.get(key).decode('utf-8'))
+        else:
+            return None
--- web/flask/datafabric.py
+++ web/flask/datafabric.py
@@ -11,6 +11,9 @@ from DatafabricManager import UserManager
 from DatafabricManager.TransactionLogging import TransactionLogging
 transaction_logging = TransactionLogging('TransactionLogs')
 
+from InternalDB.RedisDB import RedisDB
+cache_db = RedisDB(db=0)
+
 from DBMSAccessor import DBMSAccessor
 
 from RecommenderService import RecommenderService
@@ -28,48 +31,6 @@ class Config(object):
 app.config.from_object(Config())
 CORS(app)
 ''' ================ Flask Init ================ '''
-
-
-''' ================ Redis Init ================ '''
-import redis
-class RedisDB:
-    def __init__(self, host, port=6379, db=0):
-        self.__redis_db = None
-        self.__host     = host
-        self.__port     = port
-        self.__db       = db
-
-    def __connect_redis(self, retry=3) -> bool:
-        if self.__redis_db is not None:
-            return True
-        while retry >= 0:
-            try:
-                self.__redis_db = redis.Redis(host=self.__host, port=self.__port, db=self.__db)
-                return True
-            except Exception as ex:
-                if retry == 0:
-                    raise ex
-                retry -= 1
-        return False
-
-    def set_json(self, key: str, obj: dict, expire: int = None) -> bool:
-        if self.__connect_redis() == True:
-            self.__redis_db.set(key, json.dumps(obj))
-            if expire is not None:
-                self.__redis_db.expire(key, expire)
-            return True
-        else:
-            return False
-
-    def get_json(self, key: str) -> dict:
-        if self.__connect_redis() == True and self.__redis_db.exists(key):
-            return json.loads(self.__redis_db.get(key).decode('utf-8'))
-        else:
-            return None
-
-cache_db = RedisDB('datafabric-redis')
-''' ================ Redis Init ================ '''
-
 
 def validate_user() -> bool:
     '''
