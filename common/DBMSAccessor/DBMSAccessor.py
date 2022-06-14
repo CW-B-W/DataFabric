8b8297b825222bc010dc7f80a8c69ccc8113fb9a
CW-B-W <cw.brad.wang@gmail.com>
2022-06-14 05:22:01 +0000

    Add APIs for DBMSAccessor

--- common/DBMSAccessor/DBMSAccessor.py
+++ common/DBMSAccessor/DBMSAccessor.py
@@ -42,7 +42,8 @@ def query_table(
         username: str, password: str,
         ip: str, port, dbms: str, 
         db: str, table: str, columns: list, 
-        start_time: str = None, end_time: str = None, time_column: str = None) -> DataFrame:
+        start_time: str = None, end_time: str = None, time_column: str = None
+) -> DataFrame:
     """Query data from DBMS. Return pandas.DataFrame
 
     Args:
@@ -65,3 +66,74 @@ def query_table(
     # The target function name should be like such as 'query_table_mysql(...)'
     func = get_module_func(f'{dbms}Accessor', f'query_table_{dbms}')
     return func(username, password, ip, port, db, table, columns, start_time, end_time, time_column)
+
+def list_dbs(username: str, password: str, 
+    ip: str, port: str, dbms: str
+) -> list:
+    """Query what databases are in the table.
+    This is the abstract function for calling functions of desired DBMS.
+
+    Args:
+        username (str): Username of DBMS
+        password (str): Password of DBMS
+        ip (str): IP/Hostname of DBMS
+        port (str): Port of DBMS. Note that for Oracle it can be like '1521/sid'
+        dbms (str): Name of DBMS, e.g. 'MySQL', will be converted to lowercase
+
+    Returns:
+        list: List containing database names
+    """
+    dbms = dbms.lower()
+    # Use reflection to call function (Better extensibility for adding new DBMS)
+    # The target function name should be like such as 'list_dbs_mysql(...)'
+    func = get_module_func(f'{dbms}Accessor', f'list_dbs_{dbms}')
+    return func(username, password, ip, port)
+
+def list_tables(username: str, password: str, 
+    ip: str, port: str, dbms: str,
+    db: str
+) -> list:
+    """Query what tables are in the table.
+    This is the abstract function for calling functions of desired DBMS.
+
+    Args:
+        username (str): Username of DBMS
+        password (str): Password of DBMS
+        ip (str): IP/Hostname of DBMS
+        port (str): Port of DBMS. Note that for Oracle it can be like '1521/sid'
+        dbms (str): Name of DBMS, e.g. 'MySQL', will be converted to lowercase
+        db (str): Desired DB in DBMS
+
+    Returns:
+        list: List containing column names
+    """
+    dbms = dbms.lower()
+    # Use reflection to call function (Better extensibility for adding new DBMS)
+    # The target function name should be like such as 'list_columns_mysql(...)'
+    func = get_module_func(f'{dbms}Accessor', f'list_tables_{dbms}')
+    return func(username, password, ip, port, db)
+
+def list_columns(username: str, password: str, 
+    ip: str, port: str, dbms: str,
+    db: str, table: str
+) -> list:
+    """Query what columns(keys) are in the table.
+    This is the abstract function for calling functions of desired DBMS.
+
+    Args:
+        username (str): Username of DBMS
+        password (str): Password of DBMS
+        ip (str): IP/Hostname of DBMS
+        port (str): Port of DBMS. Note that for Oracle it can be like '1521/sid'
+        dbms (str): Name of DBMS, e.g. 'MySQL', will be converted to lowercase
+        db (str): Desired DB in DBMS
+        table (str): Desired Table in DB
+
+    Returns:
+        list: List containing column names
+    """
+    dbms = dbms.lower()
+    # Use reflection to call function (Better extensibility for adding new DBMS)
+    # The target function name should be like such as 'list_columns_mysql(...)'
+    func = get_module_func(f'{dbms}Accessor', f'list_columns_{dbms}')
+    return func(username, password, ip, port, db, table)
--- common/DBMSAccessor/mysqlAccessor/mysqlAccessor.py
+++ common/DBMSAccessor/mysqlAccessor/mysqlAccessor.py
@@ -18,16 +18,30 @@ def preview_table_mysql(username, password, ip, port, db, table, limit):
     return result
 
 def query_table_mysql(username, password, ip, port, db, table, columns, start_time, end_time, time_column):
+    def generate_query(table, columns, start_time, end_time, time_column):
+        req_cols = ','.join(columns)
+        optional_fields = ''
+        if start_time is not None and end_time is not None and time_column is not None:
+            optional_fields = f"WHERE {time_column} BETWEEN '{start_time}' and '{end_time}'"
+        sql = f'SELECT {req_cols} FROM {table} {optional_fields};' 
+        return sql
     sql       = generate_query(table, columns, start_time, end_time, time_column)
     db_url    = 'mysql+pymysql://%s:%s@%s:%s/%s' % (username, password, ip, port, db)
     db_engine = create_engine(db_url)
     df = pd.read_sql(sql, con=db_engine)
     return df
 
-def generate_query(table, columns, start_time, end_time, time_column):
-    req_cols = ','.join(columns)
-    optional_fields = ''
-    if start_time is not None and end_time is not None and time_column is not None:
-        optional_fields = f"WHERE {time_column} BETWEEN '{start_time}' and '{end_time}'"
-    sql = f'SELECT {req_cols} FROM {table} {optional_fields};' 
-    return sql
+def list_dbs_mysql(username, password, ip, port):
+    engine = create_engine("mysql+pymysql://%s:%s@%s:%s/" % (username, password, ip, port))
+    df = pd.read_sql("SHOW DATABASES;", con=engine)
+    return sorted(df.iloc[:,0].tolist())
+
+def list_tables_mysql(username, password, ip, port, db):
+    engine = create_engine("mysql+pymysql://%s:%s@%s:%s/%s" % (username, password, ip, port, db))
+    df = pd.read_sql("SHOW TABLES", con=engine)
+    return sorted(df.iloc[:,0].tolist())
+
+def list_columns_mysql(username, password, ip, port, db, table):
+    engine = create_engine("mysql+pymysql://%s:%s@%s:%s/%s" % (username, password, ip, port, db))
+    df = pd.read_sql("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='%s' AND `TABLE_NAME`='%s'" % (db, table), con=engine);
+    return sorted(df['COLUMN_NAME'].tolist())
