36a5895594a48bfd639b78d538a760eda1f69593
steinsgateted <steinsgateted@gmail.com>
2022-06-15 18:07:03 +0800

    add oracle

--- common/DBMSAccessor/oracleAccessor/oracleAccessor.py
+++ common/DBMSAccessor/oracleAccessor/oracleAccessor.py
@@ -5,14 +5,14 @@ import cx_Oracle
 from sqlalchemy import *
 def preview_table_oracle(username, password, ip, port, db, table, limit):
     db_engine = create_engine(r"oracle+cx_oracle://%s:%s@%s:%s" % (username, password, ip, port))
-    df = pd.read_sql("SELECT column_name FROM all_tab_cols WHERE owner = '%s' and table_name = '%s'" % (db_name, table_name), con=db_engine);
-    return sorted(df1.iloc[:,0].tolist())
+    df = pd.read_sql("SELECT column_name FROM all_tab_cols WHERE owner = '%s' and table_name = '%s'" % (db_name, table_name), con=db_engine)
+    return df
 
 def query_table_oracle(username, password, ip, port, db, table, columns, start_time, end_time, time_column):
     db_engine = create_engine(r"oracle+cx_oracle://%s:%s@%s:%s" % (username, password, ip, port))
     sql = generate_query(table, columns, start_time, end_time, time_column)
-    df = pd.read_sql("SELECT column_name FROM all_tab_cols WHERE owner = '%s' and table_name = '%s'" % (db_name, table_name), con=db_engine);
-    return sorted(df.iloc[:,0].tolist())
+    df = pd.read_sql(sql, con=db_engine)
+    return df
 
 def generate_query(table, columns, start_time, end_time, time_column):
     req_cols = ','.join(columns)
@@ -21,3 +21,18 @@ def generate_query(table, columns, start_time, end_time, time_column):
         optional_fields = f"WHERE {time_column} BETWEEN TO_DATE ('{starttime}','YYYY-MM-DD\"T\"HH24:MI:SS') AND TO_DATE('{endtime}','YYYY-MM-DD\"T\"HH24:MI:SS')"
     sql = f'SELECT {req_cols} FROM {table} {optional_fields}' 
     return sql
+
+def list_dbs_oracle(username, password, ip, port):
+    db_engine = create_engine(r"oracle+cx_oracle://%s:%s@%s:%s" % (username, password, ip, port))
+    df = pd.read_sql("select distinct OWNER from user_tab_privs", con=db_engine)
+    return sorted(df.iloc[:,0].tolist())
+
+def list_tables_oracle(db_name, username, password, ip, port):
+    db_engine = create_engine(r"oracle+cx_oracle://%s:%s@%s:%s" % (username, password, ip, port))
+    df = pd.read_sql("select table_name from user_tab_privs where OWNER = '%s'" % db_name, con=db_engine)
+    return sorted(df.iloc[:,0].tolist())
+
+def list_columns_oracle(db_name, table_name, username, password, ip, port):
+    db1_engine = create_engine(r"oracle+cx_oracle://%s:%s@%s:%s" % (username, password, ip, port))
+    df = pd.read_sql("SELECT column_name FROM all_tab_cols WHERE owner = '%s' and table_name = '%s'" % (db_name, table_name), con=db_engine)
+    return sorted(df.iloc[:,0].tolist())
