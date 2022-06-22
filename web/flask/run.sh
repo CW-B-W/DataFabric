e67f8ff542d474252d182daa8cec3a57745189c4
CW-B-W <cw.brad.wang@gmail.com>
2022-06-22 02:48:32 +0000

    Use gunicorn for multi-worker

--- web/flask/run.sh
+++ web/flask/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 trap 'kill -s INT $child' EXIT INT TERM
 
-python3 datafabric.py > datafabric.log 2>&1 &
+gunicorn --workers=4 --bind=0.0.0.0:5000 datafabric:app > datafabric.log 2>&1 &
 child=$!
 wait "$child"
