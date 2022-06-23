9d92a85e90e25ce7a0a52470f71c6d94b3be9286
steinsgateted <steinsgateted@gmail.com>
2022-06-23 11:07:30 +0800

    add hbase

--- web/flask/static/js/dbms/hbase.js
+++ web/flask/static/js/dbms/hbase.js
@@ -1,12 +1,12 @@
-function generateSrcInfo_dataframe(ip, port, username, password, dbName, tableName, columns, namemapping, startTime, endTime, timeColumn) {
+function generateSrcInfo_hbase(ip, port, username, password, dbName, tableName, columns, namemapping, startTime, endTime, timeColumn) {
     let srcInfo = {
         'dbms'        : 'hbase',
-        'ip'          : '',
-        'port'        : '',
-        'username'    : '',
-        'password'    : '',
-        'db'          : '',
-        'table'       : '',
+        'ip'          : ip,
+        'port'        : port,
+        'username'    : username,
+        'password'    : password,
+        'db'          : dbName,
+        'table'       : tableName,
         'columns'     : columns,
         'namemapping' : namemapping,
         'start_time'  : startTime,
