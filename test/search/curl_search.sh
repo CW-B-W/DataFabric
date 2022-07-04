6596b78cd41f61490fc9558aa287b0af59a36d0d
CW-B-W <cw.brad.wang@gmail.com>
2022-07-04 04:55:35 +0000

    Add blocking option for test/search

--- test/search/curl_search.sh
+++ test/search/curl_search.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-if [ ! -z $2 ]; then
+if [ ! -z $3 ]; then
     log_file=$1
     search_text=$2
     n_times=$3
     use_cache=${4:-"True"}
+    blocking=${5:-"False"}
 else
     echo -e "Usage:"
-    echo -e "\n./curl_search.sh {log_file} {search_text} {n_times} {use_cache=True}"
+    echo -e "\n./curl_search.sh {log_file} {search_text} {n_times} {use_cache=True} {blocking=False}"
     exit
 fi
 
 for ((i=0; i<$n_times; i++)); do
     ((page=i%10+1))
     # echo 'http://127.0.0.1:5000/search?text='$search_text'&page='$page
-    curl -w "@curl_format_total.txt" -o /dev/null -s 'http://127.0.0.1:5000/search?text='$search_text'&page='$page'&cache='$use_cache >> $log_file 2>&1 &
+    if [ "$blocking" == "False" ]; then
+        curl -w "@curl_format_total.txt" -o /dev/null -s 'http://127.0.0.1:5000/search?text='$search_text'&page='$page'&cache='$use_cache >> $log_file 2>&1 &
+    else
+        curl -w "@curl_format_total.txt" -o /dev/null -s 'http://127.0.0.1:5000/search?text='$search_text'&page='$page'&cache='$use_cache >> $log_file 2>&1
+    fi
 done
--- test/search/test_search.sh
+++ test/search/test_search.sh
@@ -5,13 +5,14 @@ if [ ! -z $1 ]; then
     n_proc=${2:-10}
     n_times=${3:-100}
     use_cache=${4:-"True"}
+    blocking=${5:-"False"}
 else
     echo -e "Usage:"
-    echo -e "\t./test_search.sh {search_text} {n_proc=10} {n_times=100} {use_cache=True}"
+    echo -e "\t./test_search.sh {search_text} {n_proc=10} {n_times=100} {use_cache=True} {blocking=False}"
     exit
 fi
 
 rm proc_*.log
 for ((i=0; i<$n_proc; i++)); do
-    ./curl_search.sh proc_$i.log $search_text $n_times $use_cache &
+    ./curl_search.sh proc_$i.log $search_text $n_times $use_cache $blocking &
 done
