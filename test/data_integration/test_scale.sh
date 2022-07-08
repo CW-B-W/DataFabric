f7183360f8b8fb40ce1cb4f36ed994adc53e318c
CW-B-W <cw.brad.wang@gmail.com>
2022-07-08 17:19:02 +0000

    Run data_integration test in descending order

--- test/data_integration/test_scale.sh
+++ test/data_integration/test_scale.sh
@@ -5,7 +5,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-for ((i = 1; i < $1; i += 2)); do
+for ((i = $1; i >= 1; i -= 2)); do
     docker-compose --project-name datafabric scale data-integration=$i
-    python3 test.py Join_r1310720_c10_437714537b.json -n $2 > scale_$i.log 2>&1
+    python3 test.py $3 -n $2 > scale_$i.log 2>&1
 done
