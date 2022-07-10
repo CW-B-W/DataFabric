3383fd24ab8a5b63eab6f306612dc8f2afcc9314
CW-B-W <cw.brad.wang@gmail.com>
2022-07-10 15:54:53 +0000

    Better way to clear data_integration
    The original method goes error if too many files

--- data_integration/.clear.sh
+++ data_integration/.clear.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -z "$1" ] && [ "$1" = "all" ]; then
-    rm -f data_serving/*
+    rm -rf data_serving/ && git checkout data_serving
 fi
-rm -f integration_results/*
-rm -f task_logs/*
-rm -f task_requests/*
+rm -rf integration_results/ && git checkout integration_results
+rm -rf task_logs/ && git checkout task_logs
+rm -rf task_requests/ && git checkout task_requests
